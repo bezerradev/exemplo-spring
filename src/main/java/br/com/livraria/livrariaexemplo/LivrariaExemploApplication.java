548dd751e604498a747650a3c18807c54f32c544
Mateus Bezerra <bezerra.dev@gmail.com>
2019-03-21 06:29:12 -0300

    wow

---
+++ src/main/java/br/com/livraria/livrariaexemplo/LivrariaExemploApplication.java
@@ -0,0 +1,13 @@
+package br.com.livraria.livrariaexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaExemploApplication.class, args);
+	}
+
+}
