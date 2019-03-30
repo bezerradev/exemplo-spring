56bf04a9bb199a848a1546c2f49bab41cd4606fa
iceberg20 <italoberg@yahoo.com.br>
2019-03-30 14:38:35 -0300

    1-configuracao JPA 2-api rest

--- src/main/java/br/com/livraria/livrariaexemplo/LivrariaExemploApplication.java
+++ src/main/java/br/com/livraria/livrariaexemplo/LivrariaExemploApplication.java
@@ -2,8 +2,10 @@ package br.com.livraria.livrariaexemplo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LivrariaExemploApplication {
 
 	public static void main(String[] args) {
